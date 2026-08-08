384c565124f7882ae09040c377d91d80a8f2c906
khankhanmdsher-boop <khankhanmdsher@gmail.com>
2026-08-08 07:07:33 +0300

    Disable browser caching completely

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 10000;
 
+// ब्राउज़र कैश को पूरी तरह बंद करने का Header
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    next();
+});
+
 app.use(express.static(__dirname));
 
 app.get('/admin', (req, res) => {
