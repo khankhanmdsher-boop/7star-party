79d3bf86685124ae7896fe116276d0f702d0d80e
khankhanmdsher-boop <khankhanmdsher@gmail.com>
2026-08-08 06:06:11 +0300

    Fix file location fallback

--- server.js
+++ server.js
@@ -6,14 +6,22 @@ const PORT = process.env.PORT || 10000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
 
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'), (err) => {
+        if (err) res.sendFile(path.join(__dirname, 'admin.html'));
+    });
+});
+
+app.get('/wallet', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'wallet.html'), (err) => {
+        if (err) res.sendFile(path.join(__dirname, 'wallet.html'));
+    });
+});
+
 app.get('*', (req, res) => {
-    if (req.path === '/admin') {
-        return res.sendFile(path.join(__dirname, 'public', 'admin.html'));
-    }
-    if (req.path === '/wallet') {
-        return res.sendFile(path.join(__dirname, 'public', 'wallet.html'));
-    }
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+        if (err) res.sendFile(path.join(__dirname, 'index.html'));
+    });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
