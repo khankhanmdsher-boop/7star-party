125e4ed29b7440c5a70fa9d6bdd9a1efdaa895ef
khankhanmdsher-boop <khankhanmdsher@gmail.com>
2026-08-08 06:53:30 +0300

    Fix static routing to show new coins panel

--- server.js
+++ server.js
@@ -4,24 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT || 10000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname));
 
 app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'admin.html'), (err) => {
-        if (err) res.sendFile(path.join(__dirname, 'admin.html'));
-    });
-});
-
-app.get('/wallet', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'wallet.html'), (err) => {
-        if (err) res.sendFile(path.join(__dirname, 'wallet.html'));
-    });
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
-        if (err) res.sendFile(path.join(__dirname, 'index.html'));
-    });
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
