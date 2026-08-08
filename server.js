38e073c1ce1c67d50873b7c72bc6fd11d095f849
khankhanmdsher-boop <khankhanmdsher@gmail.com>
2026-08-08 14:40:58 +0300

    Fix static path routing for index.html

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,4 +9,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 10000;
-http.listen(PORT, () => console.log('Server running on port ' + PORT));
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
